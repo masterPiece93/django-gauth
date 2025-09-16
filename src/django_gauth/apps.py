0920d960c085f8eeddfdce8163c1b64b64c3b4ed
masterPiece93 <ankit8290@gmail.com>
2025-09-16 13:06:48 +0530

    added the changes for system checks on app config for session middleware check and settings availability check ( moving out the old from views.py )

--- src/django_gauth/apps.py
+++ src/django_gauth/apps.py
@@ -1,4 +1,17 @@
 from django.apps import AppConfig   # pylint: disable=E0401
+from django.core.checks import register, Tags as DjangoTags, Warning, Info
+from django_gauth._checks import *
+from django_gauth import defaults
+import warnings; warnings.simplefilter('default')
+
+class Tags(DjangoTags):
+    """Extending with Custom Tags
+    
+    NOTE : Do this if none of the existing tags work for you:
+    https://docs.djangoproject.com/en/3.1/ref/checks/#builtin-tags
+    """
+    django_gauth_compatibility = 'django_gauth_compatibility'
+
 
 # pylint: disable=R0903
 class DjangoGauthConfig(AppConfig):
@@ -7,3 +20,70 @@ class DjangoGauthConfig(AppConfig):
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_gauth"
+
+    def ready(self):
+        register(Tags.compatibility)(check_project_middlewares)
+        register(Tags.django_gauth_compatibility)(check_project_settings)
+
+    @register(Tags.django_gauth_compatibility)
+    def set_defaults(app_configs, **kwargs):
+        errors = []
+        if not hasattr(settings, "SCOPE"):
+            setattr(settings, "SCOPE", [])
+            
+            errors.append(
+                Warning(
+                    "SCOPE setting is not defined. Defaulting to `[]`. It may affect the normal flow of oauth and might not run as expected . Please rectify ASAP.",
+                    hint=("See https://masterpiece93.github.io/django-gauth/settings/ for more information"),
+                    id=f"{DjangoGauthConfig.name}.E004" # TODO : define it consolidatedly in ErrorCodes
+                                                        # TODO : update the docs with /settings section
+                                                        #       with reference to this section
+                )
+            )
+
+        if not hasattr(settings, "GOOGLE_AUTH_FINAL_REDIRECT_URL"):
+            setattr(settings, "GOOGLE_AUTH_FINAL_REDIRECT_URL", defaults.GOOGLE_AUTH_FINAL_REDIRECT_URL)
+            _msg = f"GOOGLE_AUTH_FINAL_REDIRECT_URL settings is not defined. Defaulting to `{defaults.GOOGLE_AUTH_FINAL_REDIRECT_URL}`"
+            warnings.warn(_msg)
+        else:
+            if not settings.GOOGLE_AUTH_FINAL_REDIRECT_URL:
+                _msg = f"GOOGLE_AUTH_FINAL_REDIRECT_URL setting is set to `{settings.GOOGLE_AUTH_FINAL_REDIRECT_URL}` which is logically incorrect."
+                info=Info(
+                    _msg
+                )
+                errors.append(info)
+        if not hasattr(settings, "CREDENTIALS_SESSION_KEY_NAME"):
+            setattr(settings, "CREDENTIALS_SESSION_KEY_NAME", defaults.CREDENTIALS_SESSION_KEY_NAME)
+            _msg = f"CREDENTIALS_SESSION_KEY_NAME settings is not defined. Defaulting to `{defaults.CREDENTIALS_SESSION_KEY_NAME}`"
+            warnings.warn(_msg)
+        else:
+            if not settings.CREDENTIALS_SESSION_KEY_NAME:
+                _msg = f"CREDENTIALS_SESSION_KEY_NAME setting is set to `{settings.CREDENTIALS_SESSION_KEY_NAME}` which is logically incorrect."
+                info=Info(
+                    _msg
+                )
+                errors.append(info)
+        if not hasattr(settings, "STATE_KEY_NAME"):
+            setattr(settings, "STATE_KEY_NAME", defaults.STATE_KEY_NAME)
+            _msg = f"STATE_KEY_NAME settings is not defined. Defaulting to `{defaults.STATE_KEY_NAME}`"
+            warnings.warn(_msg)
+        else:
+            if not settings.STATE_KEY_NAME:
+                _msg = f"STATE_KEY_NAME setting is set to `{settings.STATE_KEY_NAME}` which is logically incorrect."
+                info=Info(
+                    _msg
+                )
+                errors.append(info)
+
+        if not hasattr(settings, "FINAL_REDIRECT_KEY_NAME"):
+            setattr(settings, "FINAL_REDIRECT_KEY_NAME", defaults.FINAL_REDIRECT_KEY_NAME)
+            _msg = f"FINAL_REDIRECT_KEY_NAME settings is not defined. Defaulting to `{defaults.FINAL_REDIRECT_KEY_NAME}`"
+            warnings.warn(_msg)
+        else:
+            if not settings.FINAL_REDIRECT_KEY_NAME:
+                _msg = f"FINAL_REDIRECT_KEY_NAME setting is set to `{settings.FINAL_REDIRECT_KEY_NAME}` which is logically incorrect."
+                info=Info(
+                    _msg
+                )
+                errors.append(info)
+        return errors 
--- src/django_gauth/views.py
+++ src/django_gauth/views.py
@@ -10,40 +10,7 @@ from google.auth.transport import requests      # pylint: disable=E0401
 from google.oauth2 import id_token              # pylint: disable=E0401
 from google_auth_oauthlib.flow import Flow      # pylint: disable=E0401
 
-from django_gauth import defaults
 from django_gauth.utilities import check_gauth_authentication, credentials_to_dict
-
-if hasattr(settings, "SCOPE") and settings.SCOPE:
-    SCOPE = settings.SCOPE
-else:
-    SCOPE = []
-
-if (
-    hasattr(settings, "GOOGLE_AUTH_FINAL_REDIRECT_URL")
-    and settings.GOOGLE_AUTH_FINAL_REDIRECT_URL
-):
-    GOOGLE_AUTH_FINAL_REDIRECT_URL = settings.GOOGLE_AUTH_FINAL_REDIRECT_URL
-else:
-    GOOGLE_AUTH_FINAL_REDIRECT_URL = defaults.GOOGLE_AUTH_FINAL_REDIRECT_URL
-
-if (
-    hasattr(settings, "CREDENTIALS_SESSION_KEY_NAME")
-    and settings.CREDENTIALS_SESSION_KEY_NAME
-):
-    CREDENTIALS_SESSION_KEY_NAME = settings.CREDENTIALS_SESSION_KEY_NAME
-else:
-    CREDENTIALS_SESSION_KEY_NAME = defaults.CREDENTIALS_SESSION_KEY_NAME
-
-if hasattr(settings, "STATE_KEY_NAME") and settings.STATE_KEY_NAME:
-    STATE_KEY_NAME = settings.STATE_KEY_NAME
-else:
-    STATE_KEY_NAME = defaults.STATE_KEY_NAME
-
-if hasattr(settings, "FINAL_REDIRECT_KEY_NAME") and settings.FINAL_REDIRECT_KEY_NAME:
-    FINAL_REDIRECT_KEY_NAME = settings.STATE_KEY_NAME
-else:
-    FINAL_REDIRECT_KEY_NAME = defaults.FINAL_REDIRECT_KEY_NAME
-
 
 def index(request):  # type: ignore
     is_authenticated, _ = check_gauth_authentication(request.session)
@@ -78,7 +45,7 @@ def login(request):  # type: ignore
         }
         # if you need additional scopes, add them here
         ,
-        scopes=SCOPE,
+        scopes=settings.SCOPE,
     )
 
     # flow.redirect_uri = get_redirect_uri(request) # use this when
@@ -88,13 +55,13 @@ def login(request):  # type: ignore
         access_type="offline", prompt="select_account", include_granted_scopes="true"
     )
 
-    request.session[STATE_KEY_NAME] = state
+    request.session[settings.STATE_KEY_NAME] = state
     if (
         "final_redirect" not in request.session
-        or not request.session[FINAL_REDIRECT_KEY_NAME]
+        or not request.session[settings.FINAL_REDIRECT_KEY_NAME]
     ):
-        request.session[FINAL_REDIRECT_KEY_NAME] = (
-            GOOGLE_AUTH_FINAL_REDIRECT_URL
+        request.session[settings.FINAL_REDIRECT_KEY_NAME] = (
+            settings.GOOGLE_AUTH_FINAL_REDIRECT_URL
             or request.build_absolute_uri(reverse("django_gauth:index"))
         )  # directs where to land after login is successful.
 
@@ -106,7 +73,7 @@ def callback(request):  # type: ignore
     - Google IDP response control transfer
     """
     # pull the state from the session
-    session_state = request.session.get(STATE_KEY_NAME)
+    session_state = request.session.get(settings.STATE_KEY_NAME)
     redirect_uri = request.build_absolute_uri(reverse("django_gauth:callback"))
     authorization_response = request.build_absolute_uri()
     # Flow Creation
@@ -142,8 +109,8 @@ def callback(request):  # type: ignore
     )
     # session setting
     request.session["id_info"] = id_info
-    request.session[CREDENTIALS_SESSION_KEY_NAME] = credentials_to_dict(credentials)
+    request.session[settings.CREDENTIALS_SESSION_KEY_NAME] = credentials_to_dict(credentials)
     # redirecting to the final redirect (i.e., logged in page)
-    redirect_response = redirect(request.session[FINAL_REDIRECT_KEY_NAME])
+    redirect_response = redirect(request.session[settings.FINAL_REDIRECT_KEY_NAME])
 
     return redirect_response
